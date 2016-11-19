31c24e1d2e0bfa2008987e0f3bc9278bb49dcab6
Toan Nguyen <toannguyen1@gmail.com>
2016-11-19 16:13:36 -0500

    Added Ball actor to PlayState.

--- core/src/com/mygdx/game/PlayActors/Ball.java
+++ core/src/com/mygdx/game/PlayActors/Ball.java
@@ -1,11 +1,22 @@
 package com.mygdx.game.PlayActors;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
 /**
  * Created by Toan on 11/19/2016.
  */
 
-public class Ball {
-    public Ball() {
+public class Ball extends Actor{
+    Texture ballTexture;
 
+    public Ball() {
+        ballTexture = new Texture("ball.png");
+    }
+
+    @Override
+    public void draw (Batch sb, float parentAlpha) {
+        sb.draw(ballTexture, 300, 300);
     }
 }
