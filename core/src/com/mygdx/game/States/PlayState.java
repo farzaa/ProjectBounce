ffdf25698a328a5fe6f823e8951789a39b4702a7
Farzain Majeed <contactclothinghousenow@gmail.com>
2016-11-21 17:39:31 -0500

    Changed play state to init box2d and draw basic edges

--- core/src/com/mygdx/game/States/PlayState.java
+++ core/src/com/mygdx/game/States/PlayState.java
@@ -1,23 +1,30 @@
 package com.mygdx.game.States;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.game.GameDemo;
@@ -32,55 +39,89 @@ import java.util.Random;
  * Created by flynn on 11/16/16.
  */
 
+
 public class PlayState extends State {
+    public static final float GRAVITY = 5;
+    public static final float PIXELS_TO_METERS = 100f;
 
-    private Stage playStateStage;
-    private PlayStateHUD playStateHUD;
-    private Bar bar1;
-    private ArrayList<Ball> ballList;
+    World world;
+    Camera camera;
+    Box2DDebugRenderer debugRenderer;
+    Matrix4 debugMatrix;
 
-    static final float SCALE = 0.05f;
+    Texture playstateHUD;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        playStateStage = new Stage(new ScreenViewport());
-        playStateHUD = new PlayStateHUD();
-        playStateStage.addActor(playStateHUD);
-        bar1 = new Bar();
-        playStateStage.addActor(bar1);
-        Gdx.input.setInputProcessor(playStateStage);
-        ballList = new ArrayList<Ball>();
-        spawnBalls();
+        //Box2D.init();
+
+        playstateHUD = new Texture("playstate-hud.png");
+        //First actually create World and add basic boundaries.
+        initWorld();
+        //Gdx.input.setInputProcessor(this);
+        debugRenderer = new Box2DDebugRenderer();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
     }
 
+    public void initWorld() {
+        //Create world, give it gravity.
+        world = new World(new Vector2(0, GRAVITY), true);
+        float w = Gdx.graphics.getWidth()/PIXELS_TO_METERS;
+        float h = Gdx.graphics.getHeight()/PIXELS_TO_METERS - 20/PIXELS_TO_METERS;
+        //Now create the boundaries. Pass it the vertexes.
+        //bottom edge
+        createEdge(-w/2,-h/2,w/2,-h/2);
+        //left edge.
+        createEdge(-w/2,-h/2,-w/2,h/2);
+        //right edge
+        createEdge(w/2,-h/2,w/2,h/2);
+
+        //TO DO: top edge.
+    }
+
+    public void createEdge (float vx1, float vy1, float vx2, float vy2){
+        BodyDef edgeInitial = new BodyDef();
+        //StaticBody because our edges will not move.
+        edgeInitial.type = BodyDef.BodyType.StaticBody;
+        edgeInitial.position.set(0,0);
+
+        EdgeShape edgeShape = new EdgeShape();
+        edgeShape.set(vx1, vy1, vx2, vy2);
+        FixtureDef leftEdgeFix = new FixtureDef();
+        leftEdgeFix.shape = edgeShape;
+
+        Body edgeFinal = world.createBody(edgeInitial);
+        edgeFinal.createFixture(leftEdgeFix);
+
+        edgeShape.dispose();
+    }
     @Override
     protected void handleInput() {
 
     }
 
-    public void spawnBalls() {
-        for(int i = 0; i < 5; i++) {
-            Vector2 vector = new Vector2();
-            vector.set(500, 500);
-            Ball ball = new Ball(vector);
-            ballList.add(ball);
-            playStateStage.addActor(ball);
-        }
-    }
     @Override
     protected void update(float dt) {
-        handleInput();
-        if(ballList.size() < 10) {
-            spawnBalls();
-        }
-        for(int i = 0; i < ballList.size(); i++) {
-            ballList.get(i).update();
-        }
+
     }
 
     @Override
     protected void render(SpriteBatch sb) {
-        playStateStage.draw();
-        playStateStage.act(Gdx.graphics.getDeltaTime());
+        camera.update();
+        // Step the physics simulation forward at a rate of 60hz
+        world.step(1f/60f, 6, 2);
+
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        sb.setProjectionMatrix(camera.combined);
+        debugMatrix = sb.getProjectionMatrix().cpy().scale(PIXELS_TO_METERS,
+                PIXELS_TO_METERS, 0);
+
+        sb.begin();
+        sb.draw(playstateHUD, -540, -960);
+
+        sb.end();
+        debugRenderer.render(world, debugMatrix);
     }
 }
